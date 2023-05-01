dd22c40ce3a25c36f334086dfe4c29cc639128be
nengkya <nengkya@yahoo.com>
2023-05-01 12:30:30 +0700

    locator

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 https://www.selenium.dev/documentation/webdriver/getting_started/first_script/
+
+https://www.google.com/search?q=selenium+find+hidden+element+python&oq=selenium+find+hidden+element+python&aqs=chrome..69i57.9442j0j4&sourceid=chrome&ie=UTF-8
+
+https://selenium-python.readthedocs.io/locating-elements.html
--- selenium_web_form.py
+++ selenium_web_form.py
@@ -1,6 +1,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+#for input text in html form
+from selenium.webdriver.common.keys import Keys
 
 #import Action chains for pause
 from selenium.webdriver.common.action_chains import ActionChains
@@ -8,33 +10,43 @@ from selenium.webdriver.common.action_chains import ActionChains
 def test_eight_components():
 	driver = webdriver.Chrome()
 
-	driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-	#driver.get('https://www.customs.go.th/statistic_report.php?lang=en&')
+	#driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+	driver.get('https://www.customs.go.th/statistic_report.php?lang=en&')
 
 	title = driver.title
-	#assert title == "Thai Customs"
-	assert title == "Web Form"
+	assert title == "Thai Customs"
+	#assert title == "Web form"
 
-	#driver.implicitly_wait(0.5)
+	driver.implicitly_wait(0.5)
 
-	text_box = driver.find_element(by=By.NAME, value="my-text")
+	#text_box = driver.find_element(by=By.NAME, value="my-text")
+	text_box = driver.find_element(by=By.NAME, value="tariff-code")
 
-	submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+	#submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+	submit_button = driver.find_element(by=By.CLASS_NAME, value="btn btn-success")
+
+	#text_box.send_keys("Selenium")
+	text_box.send_keys("")
+
 
 	#create action chain object
 	action = ActionChains(driver)
 
-	action.pause(15)
+	action.pause(5)
 
 	#perform the operation
 	action.perform()
 
-	text_box.send_keys("Selenium")
 	submit_button.click()
 
 	message = driver.find_element(by=By.ID, value="message")
 	value = message.text
 	assert value == "Received!"
+
+	action.pause(5)
+
+	#perform the operation
+	action.perform()
 
 	driver.quit()
 
